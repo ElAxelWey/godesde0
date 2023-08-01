9f1f85dd907155fb3d1686ca5e85e28ca76d40ab
ElAxelWey <adrielgigena8@gmail.com>
2023-08-01 16:48:52 -0300

    Siguiente Commit

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"fmt"
+
 	variables "github.com/ElAxelWey/godesde0/Variables"
 )
 
 func main() {
-	variables.MuestroEnteros()
+	estado, texto := variables.ConviertoATexto(88)
+	fmt.Println(estado)
+	fmt.Println(texto)
 }
